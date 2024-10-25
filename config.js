cd894010354df617647967c199dd7f18374efaa0
Cleber F Rodrigues <65691983+py3tx@users.noreply.github.com>
2024-10-25 00:26:14 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-const topBarCenterText = `VA3HDL - FN04ga`;
+const topBarCenterText = `PY3TX - GG40wb`;
 // Menu items
 // Structure is as follows HTML Color code, Option, target URL, scaling 1=Original Size, side (optional, nothing is Left, "R" is Right)
 // The values are [color code, menu text, target link, scale factor, side],
