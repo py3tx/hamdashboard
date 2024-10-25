8f8cca10a384dc9814a855c0d4cf70da9c2085a2
Cleber F Rodrigues <65691983+py3tx@users.noreply.github.com>
2024-10-25 17:32:11 -0300

    Update config.js

--- config.js
+++ config.js
@@ -75,7 +75,7 @@ const aIMG = [
   ],
   [
     "SATELLITE CGL",
-    "https://cdn.star.nesdis.noaa.gov/GOES16/ABI/SECTOR/ssa/13/GOES16-SSA-13-900x540.giff",
+    "https://cdn.star.nesdis.noaa.gov/GOES16/ABI/SECTOR/ssa/13/GOES16-SSA-13-900x540.gif",
   ],
   [
     "SOLAR FLARES",
