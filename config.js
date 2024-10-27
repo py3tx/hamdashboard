338c89042a059decc298732ec7e9c55a5b4c3bac
Cleber F Rodrigues <65691983+py3tx@users.noreply.github.com>
2024-10-26 23:30:53 -0300

    Update config.js

--- config.js
+++ config.js
@@ -57,7 +57,7 @@ const aURL = [
 const aIMG = [
   [
     "RADAR",
-    "iframe|https://www.meteoblue.com/pt/tempo/mapas/widget?windAnimation=1&gust=1&satellite=1&cloudsAndPrecipitation=1&temperature=1&sunshine=1&extremeForecastIndex=1&geoloc=detect&tempunit=C&windunit=km%252Fh&lengthunit=metric&zoom=4&autowidth=auto"  frameborder="0" scrolling="NO" allowtransparency="true" sandbox="allow-same-origin allow-scripts allow-popups allow-popups-to-escape-sandbox" style="width: 100%; height: 100%"],
+    "iframe|https://www.meteoblue.com/pt/tempo/mapas/widget?windAnimation=1&gust=1&satellite=1&cloudsAndPrecipitation=1&temperature=1&sunshine=1&extremeForecastIndex=1&geoloc=detect&tempunit=C&windunit=km%252Fh&lengthunit=metric&zoom=4&autowidth=auto",
   [
     "LOCAL RADAR",
     "https://ciram.epagri.sc.gov.br/mapas/Chuva_24h/prec24h.png",
